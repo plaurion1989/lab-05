91af6b5a9b49dfbd7ef733d4ac135cd8c40ca8bb
plaurion1989 <plaurion@protonmail.com>
2021-08-20 13:20:47 -0700

    created new lab-05 app with starter code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react'
+import Header from './components/header'
+import CreateForm from './components/create-form'
+import ReportTable from './components/report-table'
+import Footer from './components/footer'
+import { hours } from './data'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      standReports: []
+    }
+  }
+
+  handleCreate = (standData) => {
+    const standReports = [...this.state.standReports, standData];
+    this.setState({ standReports });
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <main>
+          <CreateForm onCreate={this.handleCreate} />
+          <ReportTable hours={hours} reports={this.state.standReports} />
+        </main>
+        <Footer reports={this.state.standReports} />
+      </div >
+    );
+  }
 }
 
 export default App;
